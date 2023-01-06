01abef6e4b8ec77f7f952f3d033c737f60c29c1e
Ricky <ricky@ricky.com>
2023-01-05 19:35:47 -0600

    modified:   src/org/launchcode/java/demos/lsn1datatypes/HelloMethods.java 	modified:   src/org/launchcode/java/demos/lsn1datatypes/Message.java 	modified:   src/org/launchcode/java/demos/lsn1datatypes/TempConverter.java 	new file:   src/org/launchcode/java/studios/areaofacircle/Area.java 	new file:   src/org/launchcode/java/studios/areaofacircle/Circle.java

---
+++ src/org/launchcode/java/studios/areaofacircle/Circle.java
@@ -0,0 +1,9 @@
+package org.launchcode.java.studios.areaofacircle;
+
+import java.lang.Math;
+
+public class Circle {
+    public static Double getArea(Double radius){
+        return Math.PI * Math.pow(radius, 2.0);
+    }
+}
